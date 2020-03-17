a013a196f32c5589819675104c608d84dfe59433
Felix Godt <felixgodt@gmx.de>
2020-03-17 19:42:25 +0100

    Array Fix

--- ZionHost/config.hpp
+++ ZionHost/config.hpp
@@ -7,9 +7,9 @@
 */
 
       //Fahrzeuge die EMP haben sollen.
-      emp[] = {
+      emp = [
         "B_MRAP_01_hmg_F",
         "B_Heli_Light_01_F",
         "B_Heli_Transport_01_F",
         "I_Heli_light_03_unarmed_F"
-      };
+            ]
