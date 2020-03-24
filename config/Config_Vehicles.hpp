67d3f61c4e585cd987fcc59477d33d61e9fcc995
ZHNiggo <nico99.3@web.de>
2020-03-24 19:16:39 +0100

    fix

--- config/Config_Vehicles.hpp
+++ config/Config_Vehicles.hpp
@@ -1193,7 +1193,7 @@ will modify the virtual space and the price of the vehicle, but other informatio
         price = 1;
         textures[] = {
             { "SEK", "civ", {
-                "textures\sek\guav.jpg"
+                "textures\sek\drohne.jpg"
             }, "" }
         };
     };
