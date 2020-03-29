84a18ac1012bfb56fff4e096a14f20d580386d79
Felix Godt <felixgodt@gmx.de>
2020-03-29 04:53:09 +0200

    config added

---
+++ config/Config_Zionhost.hpp
@@ -0,0 +1,14 @@
+/*
+
+			PROPERTY OF THE COMPANY: ZIONHOST UG
+
+		USING THIS WITHOUT DIRECT ACCESS IS FORBIDDEN
+
+*/
+#define true 1
+#define false 0
+class ZionHost_Clans {
+  class TH2D {
+    doors = [CLAN_TH2D_DOOR_01,CLAN_TH2D_DOOR_02,CLAN_TH2D_DOOR_03];
+  };
+};
